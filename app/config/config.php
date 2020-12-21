cff7178373ea0229695ad75eac53656a30397f4b
ISK <iskoroom@gmail.com>
2020-12-21 23:24:12 +0900

    firtst commit

---
+++ app/config/config.php
@@ -0,0 +1,37 @@
+<?php
+
+// ☆☆☆
+// 追加で public\.htaccess の ルートURLも変更するように気をつける
+// ☆☆☆
+
+// ☆☆☆
+// session_helper.php の以下も変更しました（フォーム再送信の確認の回避のため）
+// session_start();
+// header('Expires:-1');
+// header('Cache-Control:');
+// header('Pragma:');
+// ☆☆☆
+
+
+  // DB Params
+  define('DB_HOST', 'localhost');
+  define('DB_USER', 'root');
+  define('DB_PASS', '');
+  define('DB_NAME', 'SharePosts');
+
+  // App Root
+  // __FILE__ → config.phpのファイルパス
+  // dirname(__FILE__) → config.php が存在するディレクトリのファイルパス
+  // dirname(dirname(__FILE__)) → app フォルダのファイルパス
+  define('APPROOT',dirname(dirname(__FILE__)));
+
+  // URL ROOT
+  define('URLROOT', 'http://localhost/shareposts');
+
+  // Site Name
+  define('SITENAME', 'SharePosts');
+
+  // App version
+  define('APPVERSION', '1.0.0');
+
+?>
---
+++ app/controllers/Pages.php
@@ -0,0 +1,34 @@
+<?php
+  class Pages extends Controller {
+
+    function __construct(){
+    }
+
+    public function index(){
+
+      if (isLoggedIn()) {
+        redirect('posts');
+      }
+
+      $data = [
+        'title'=>'SharePosts',
+        'description' => 'Simple social network built on the TraversyMVC PHP framework'
+      ];
+
+      // class Controller を継承してるから view() を呼べる
+      $this->view('pages/index',$data);
+    }
+
+    public function about(){
+      $data = [
+        'title'=>'About',
+        'description' => 'App to share posts with other users.'
+      ];
+      $this->view('pages/about', $data);
+    }
+  }
+
+
+
+
+?>
